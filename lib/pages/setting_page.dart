bfe4f6a2d3e17cfaaa846491e5ad2878c7c41b1d
ketchup0211 <dlcka1111@naver.com>
2023-11-11 04:43:45 +0900

    setting_page.dart 버튼 구현중

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -12,8 +12,95 @@ class _SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('부가 가능들'),
+          title: const Center(child: Text('부가 가능들')),
+          backgroundColor: Colors.black,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 50,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    width: 330,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            '설정',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 1.15,
+                            child: Transform.translate(
+                              offset: const Offset(2.0, 23.0),
+                              child:
+                                  Image.asset('assets/icons/setting_color.png'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    width: 330,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            '불법주차\n신고하기',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Transform.scale(
+                            scale: 0.8,
+                            child: Transform.translate(
+                              offset: const Offset(2.0, 23.0),
+                              child:
+                                  Image.asset('assets/icons/report_color.png'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
