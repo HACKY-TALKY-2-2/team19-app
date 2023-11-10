e71c712a19a0e6d3ce1490a76e74b05ecf32cff2
skykhs3 <skykhs3@naver.com>
2023-11-11 02:14:29 +0900

    Feat(main_provider.dart): 프로바이더 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,27 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:parking_app/pages/main_page.dart';
+import 'package:parking_app/providers/main_provider.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MainPage(),
+    return MultiProvider(
+      
+      providers: [
+        ChangeNotifierProvider(create: (_) => MainProvider())
+      ],
+      child: const MaterialApp(
+        title: 'Flutter Google Maps Demo',
+        home: MainPage(),
+      ),
     );
   }
 }
